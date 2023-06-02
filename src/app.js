d3f01d805b0c1f3b73dee119572797e7a1617e9f
Leonardo Costa <112534103+Justice3001@users.noreply.github.com>
2023-06-02 23:42:09 +0000

    first commit.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateDomainName() {
+  const keywords = [
+    "tower",
+    "action",
+    "knight",
+    "purpose",
+    "candle",
+    "time",
+    "energy"
+  ]; // Add your own keywords
+
+  const endings = [".com", ".gov", ".us", ".ca", ".net", ".org", "io", ".biz"]; // endings
+  const begginning = ["www."]; //beginning
+
+  const randomKeyword = keywords[Math.floor(Math.random() * keywords.length)];
+  const randomEnding = endings[Math.floor(Math.random() * endings.length)];
+
+  return begginning + randomKeyword + randomEnding;
+}
+
+//output 5 different names
+for (var i = 0; i < 5; i++) {
+  console.log(generateDomainName());
+}
